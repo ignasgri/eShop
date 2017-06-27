6c76f89014b83c88a3e2e4b08c31fd1b32671101
Ignas Grigaravicius <kickeris@gmail.com>
2017-06-27 12:06:44 +0100

    Settings changed

--- eshop/settings.py
+++ eshop/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9icu)bj8crbuiui3%^@mc(qhp0fms#%x85o@)7n2glz6(_%%i+'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'eshop',
+    'home',
 ]
 
 MIDDLEWARE = [
